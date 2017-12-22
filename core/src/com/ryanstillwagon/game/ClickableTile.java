10102f00455b8eb187468c86dccb39262a323526
ryanstills <stillwagonryan@gmail.com>
2017-12-22 12:04:30 -0800

    map tools now properly work, added export button

--- core/src/com/ryanstillwagon/game/ClickableTile.java
+++ core/src/com/ryanstillwagon/game/ClickableTile.java
@@ -1,24 +1,39 @@
 package com.ryanstillwagon.game;
 
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
-public class ClickableTile{
-    static Map<String, ClickableTile> tileHashMap = new HashMap<String, ClickableTile>();
+public class ClickableTile extends Actor{
     Sprite sprite;
-    int iconListPosition;
-    ClickableTile(Resources res){
+    private int iconListPosition;
+    ClickableTile(final Resources res){
         iconListPosition = 0;
         sprite = new Sprite(res.iconList[iconListPosition]);
-        tileHashMap.put(stringify(), this);
+
+        addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+                if(event.getButton() == Input.Buttons.LEFT) {
+                    if (iconListPosition == res.iconList.length - 1) {
+                        iconListPosition = 0;
+                        sprite.setTexture(res.iconList[iconListPosition]);
+                    } else {
+                        iconListPosition++;
+                        sprite.setTexture(res.iconList[iconListPosition]);
+                    }
+                }
+            }
+        });
     }
-
-
-
-    String stringify(){
-        return Integer.toString((int)sprite.getX(), (int)sprite.getY());
+    public int getValue(){
+        return iconListPosition;
+    }
+    public void draw(Batch batch, float parentAlpha){
+        sprite.draw(batch);
     }
 }
--- core/src/com/ryanstillwagon/game/MapTool.java
+++ core/src/com/ryanstillwagon/game/MapTool.java
@@ -2,25 +2,32 @@ package com.ryanstillwagon.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class MapTool extends ApplicationAdapter {
-	SpriteBatch batch;
-	OrthographicCamera camera;
-	TiledMapRenderer levelRenderer;
-	Resources res;
-	ArrayList<ClickableTile> tileList;
-	int winHeight;
-	int winWidth;
-	int clickX;
-	int clickY;
+	private SpriteBatch batch;
+	private Resources res;
+	private Stage stage;
+	private Button button;
+	private Skin buttonSkin;
+	private ArrayList<ClickableTile> tileList;
+
+	private int winHeight;
+	private int winWidth;
+	 private int mapID = 0;
 
 	private void loadGrid(){
 	    for(int y = res.TILE_SIZE; y < winHeight - res.TILE_SIZE; y += res.TILE_SIZE){
@@ -28,49 +35,49 @@ public class MapTool extends ApplicationAdapter {
 	            ClickableTile temp = new ClickableTile(res);
 	            temp.sprite.setPosition(x, y);
 	            tileList.add(temp);
+	            temp.setBounds(temp.sprite.getX(), temp.sprite.getY(), temp.sprite.getWidth(), temp.sprite.getHeight());
+	            temp.setTouchable(Touchable.enabled);
+	            stage.addActor(temp);
             }
-        }
-    }
-    private void renderGrid(){
-	    for(ClickableTile tile: tileList){
-	        batch.draw(tile.sprite, tile.sprite.getX(), tile.sprite.getY());
-	    }
-    }
-    private void checkForMouseClick(){
-        if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-
         }
     }
     @Override
 	public void create () {
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+		buttonSkin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+		mapID = new File("C:\\Users\\still\\IdeaProjects\\first-tutorial\\android\\assets\\maps").listFiles().length + 1;
+
 		batch = new SpriteBatch();
 		res = new Resources();
-		levelRenderer = new OrthogonalTiledMapRenderer(res.background);
+		button = new TextButton("Export", buttonSkin, "small");
+		button.setSize(res.TILE_SIZE * 2, res.TILE_SIZE);
+		button.setPosition(0,0);
+		button.setTransform(true);
+		button.scaleBy(0.025f);
+		button.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y){
+				FileHandle exportFile = Gdx.files.absolute("C:\\Users\\still\\IdeaProjects\\first-tutorial\\android\\assets\\maps\\level"+mapID+".txt");
+				for(int i = 0; i < tileList.size(); i++ ) {
+					exportFile.writeString(Integer.toString(tileList.get(i).getValue()), true);
+				}
+			}
+		});
 		tileList = new ArrayList<ClickableTile>();
 
 		winWidth = Gdx.graphics.getWidth();
 		winHeight = Gdx.graphics.getHeight();
 
 		loadGrid();
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, winWidth, winHeight);
-		camera.update();
+		stage.addActor(button);
 	}
-
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-        levelRenderer.setView(camera);
-        levelRenderer.render();
-		batch.begin();
-            renderGrid();
-		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
